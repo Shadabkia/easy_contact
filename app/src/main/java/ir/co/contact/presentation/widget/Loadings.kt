86edae92854290de2b7ca70cebad0040b35bf00d
PERSIANSWITCH\h.shadabkia <hr.shadabkia@gmail.com>
2025-11-09 12:59:43 +0330

    update loading ui

--- app/src/main/java/ir/co/contact/presentation/widget/Loadings.kt
+++ app/src/main/java/ir/co/contact/presentation/widget/Loadings.kt
@@ -1,37 +1,56 @@
 package ir.co.contact.presentation.widget
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.room.util.TableInfo
+import ir.co.contact.R
 
 @Composable
 fun ScreenLoading(
     modifier: Modifier,
     transparent: Boolean = false
 ) {
-    Box(
+    Column(
         modifier = modifier.fillMaxSize()
         .background(if(transparent) Color.Transparent else MaterialTheme.colorScheme.background),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
     ) {
         CircularProgressIndicator(
             modifier = modifier
                 .width(50.dp)
                 .height(50.dp)
-                .align(Alignment.Center),
-            strokeWidth = 2.dp
+                .align(Alignment.CenterHorizontally),
+            strokeWidth = 4.dp
         )
-    }
+
+        Text(
+            text = stringResource(R.string.loading),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp, vertical = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        )    }
 }
 
 
@@ -39,6 +58,6 @@ fun ScreenLoading(
 @Preview
 @Composable
 fun ScreenLoadingPreview(){
-    ScreenLoading(Modifier, true)
+    ScreenLoading(Modifier, false)
 }
 
