8e458aebb5854df1d8b8f7c7a5f05c38c6740513
PERSIANSWITCH\h.shadabkia <hr.shadabkia@gmail.com>
2025-11-09 14:45:52 +0330

    efficient delete and inserting data in database by upsert

--- app/src/main/java/ir/co/contact/data/source/local/database/ContactDao.kt
+++ app/src/main/java/ir/co/contact/data/source/local/database/ContactDao.kt
@@ -22,11 +22,20 @@ interface ContactDao {
     @Query("DELETE FROM contacts")
     suspend fun clearContacts()
 
+    @Query("DELETE FROM contacts WHERE id NOT IN (:contactIds)")
+    suspend fun deleteContactsNotIn(contactIds: List<String>)
+
     @Transaction
     suspend fun replaceContacts(contacts: List<ContactEntity>) {
-        clearContacts()
         if (contacts.isNotEmpty()) {
+            // UPSERT: Insert new contacts or update existing ones
             insertContacts(contacts)
+
+            val currentContactIds = contacts.map { it.id }
+            deleteContactsNotIn(currentContactIds)
+        } else {
+            // If no contacts on phone, clear all
+            clearContacts()
         }
     }
 }
